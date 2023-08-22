467da56eff458b009c42db1b8403fbfff3a1b324
Roman Savenko <rsaven.wk@gmail.com>
2023-08-22 15:28:21 +0300

    feat: start project

---
+++ BookLib/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
