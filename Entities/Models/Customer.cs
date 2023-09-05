2e4725309c58d0d3af0957112f33fb6ba36d3669
Roman Savenko <rsaven.wk@gmail.com>
2023-09-05 14:29:09 +0300

    fix: remove base user model

--- Entities/Models/Customer.cs
+++ Entities/Models/Customer.cs
@@ -1,11 +1,14 @@
 namespace Entities.Models;
 
-public class Customer : BaseUser
+public class Customer
 {
+    public long CustomerId { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string ContactNumber { get; set; } = String.Empty;
     public string Address { get; set; } = string.Empty;
+    public DateTime CreatedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
     ICollection<CustomerBook> CustomerBooks { get; set; } = new List<CustomerBook>();
 }
