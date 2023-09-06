e921b17b4cb51c8595cb8c3583175f5179412f4f
Roman Savenko <rsaven.wk@gmail.com>
2023-09-06 12:58:12 +0300

    feat: add customer seed sql script

---
+++ BookLib/SqlScripts/Script0004 - CustomerSeed.sql
@@ -0,0 +1,13 @@
+-- Active: 1693557362053@@127.0.0.1@5432@book_lib_db
+INSERT INTO "customers" ("first_name", "last_name", "email", "contact_number", "address", "created_date", "modified_date")
+VALUES
+    ('John', 'Doe', 'john.doe@example.com', '123-456-7890', '123 Main St, Anytown, USA', '2023-09-06', '2023-09-06'),
+    ('Jane', 'Smith', 'jane.smith@example.com', '987-654-3210', '456 Elm St, Othertown, USA', '2023-09-06', NULL),
+    ('Alice', 'Johnson', 'alice.johnson@example.com', '555-555-5555', '789 Oak St, Anothertown, USA', '2023-09-06', NULL),
+    ('Bob', 'Williams', 'bob.williams@example.com', '111-222-3333', '101 Pine St, Somewhere, USA', '2023-09-06', NULL),
+    ('Eve', 'Brown', 'eve.brown@example.com', '999-888-7777', '202 Cedar St, Nowhere, USA', '2023-09-06', NULL),
+    ('David', 'Lee', 'david.lee@example.com', '777-888-9999', '303 Birch St, Elsewhere, USA', '2023-09-06', NULL),
+    ('Grace', 'Turner', 'grace.turner@example.com', '444-333-2222', '505 Maple St, Anyplace, USA', '2023-09-06', NULL),
+    ('Michael', 'Adams', 'michael.adams@example.com', '666-777-8888', '606 Walnut St, Noway, USA', '2023-09-06', NULL),
+    ('Olivia', 'Martin', 'olivia.martin@example.com', '222-333-4444', '707 Cherry St, Everytown, USA', '2023-09-06', NULL),
+    ('William', 'Taylor', 'william.taylor@example.com', '555-444-3333', '808 Spruce St, Anyville, USA', '2023-09-06', NULL);
