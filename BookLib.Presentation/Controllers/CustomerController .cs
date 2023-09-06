5b012c804b8179e3245c9f8aaf86c4d8e1ba929a
Roman Savenko <rsaven.wk@gmail.com>
2023-09-06 12:55:21 +0300

    feat: add customer controller

---
+++ BookLib.Presentation/Controllers/CustomerController .cs
@@ -0,0 +1,54 @@
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.DataTransferObjects;
+
+namespace BookLib.Presentation.Controllers;
+
+[Route("api/customers")]
+[ApiController]
+public class CustomerController : ControllerBase
+{
+    private readonly IServiceManager _manager;
+    public CustomerController(IServiceManager manager)
+    {
+        _manager = manager;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllCustomers()
+    {
+        var customers = await _manager.CustomerService.GetAllCustomers();
+        return Ok(customers.OrderBy(customer => customer.CustomerId));
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddCustomer([FromBody] CustomerForAddDto customer)
+    {
+        await _manager.CustomerService.AddCustomer(customer);
+        return Ok();
+    }
+
+    [Route("{id}")]
+    [HttpGet]
+    public async Task<IActionResult> GetCustomer(long id)
+    {
+        var customer = await _manager.CustomerService.GetCustomer(id);
+        return Ok(customer);
+    }
+
+    [Route("{id}")]
+    [HttpDelete]
+    public async Task<IActionResult> DeleteCustomer(long id)
+    {
+        await _manager.CustomerService.DeleteCustomer(id);
+        return Ok();
+    }
+
+    [Route("{id}")]
+    [HttpPut]
+    public async Task<IActionResult> UpdateCustomer(long id, [FromBody] CustomerForUpdateDto customer)
+    {
+        await _manager.CustomerService.UpdateCustomer(id, customer);
+        return Ok();
+    }
+}
