fffca926bc58e391aff167575e739ddd0e187015
Roman Savenko <rsaven.wk@gmail.com>
2023-09-18 22:19:41 +0300

    refactor: update customer book repo

--- Contracts/ICustomerBookRepository.cs
+++ Contracts/ICustomerBookRepository.cs
@@ -1,12 +1,14 @@
 using Entities.Models;
-using Shared.DataTransferObjects;
+using Shared.DataTransferObjects.Book;
+using Shared.DataTransferObjects.CustomerBook;
 
 namespace Contracts;
 
 public interface ICustomerBookRepository
 {
-    Task<IEnumerable<CustomerBookDto>> GetAllCustomerBooks(long customerId);
-    Task<CustomerBookDto> GetCustomerBook(long customerId, long bookId);
-    Task AddCustomerBook(long customerId, CustomerBookDto book);
-    Task DeleteCustomerBook(long customerId, long bookId);
+    Task<IEnumerable<ExtendBookDto>> GetAllCustomerBooks(long customerId);
+    Task<ExtendBookDto> GetCustomerBook(long customerId, long bookId);
+    void AddCustomerBook(long customerId, CustomerBookDto book);
+    void DeleteCustomerBook(long customerId, long bookId);
+    Task<bool> CustomerBookExists(long customerId, long bookId);
 }
--- Repository/CustomerBookRepository.cs
+++ Repository/CustomerBookRepository.cs
@@ -1,7 +1,8 @@
 using Contracts;
 using Entities.Models;
-using Shared.DataTransferObjects;
 using Dapper;
+using Shared.DataTransferObjects.CustomerBook;
+using Shared.DataTransferObjects.Book;
 
 namespace Repository;
 
@@ -14,7 +15,7 @@ public class CustomerBookRepository : ICustomerBookRepository
         _context = context;
     }
 
-    public async Task AddCustomerBook(long customerId, CustomerBookDto book)
+    public void AddCustomerBook(long customerId, CustomerBookDto book)
     {
         var query = @$"INSERT INTO customer_books
                     (customer_id, book_id) 
@@ -22,36 +23,84 @@ public class CustomerBookRepository : ICustomerBookRepository
                     (@customerId, @bookId)";
         using var connection = _context.CreateConnection();
         var temp = new CustomerBook {BookId = book.BookId, CustomerId = customerId};
-        await connection.ExecuteAsync(query, temp);
+        connection.Execute(query, temp);
     }
- 
-    public async Task DeleteCustomerBook(long customerId, long bookId)
+
+    public async Task<bool> CustomerBookExists(long customerId, long bookId)
+    {
+        var query = @"SELECT *
+                    FROM
+                        customer_books
+                    WHERE 
+                        customer_id = @customerId 
+                        AND book_id = @bookId";
+        using var connection = _context.CreateConnection();
+        var temp = new CustomerBook {BookId = bookId, CustomerId = customerId};
+        var customerBook = await connection.QueryFirstOrDefaultAsync<CustomerBook>(query, temp);
+        return customerBook is not null;
+    }
+
+    public void DeleteCustomerBook(long customerId, long bookId)
     {
         var query = @"DELETE FROM customer_books
                     WHERE customer_id = @customerId
                     AND book_id = @bookId";
         using var connection = _context.CreateConnection();
         var temp = new CustomerBook {BookId = bookId, CustomerId = customerId};
-        await connection.ExecuteAsync(query, temp);
+        connection.Execute(query, temp);
     }
 
-    public async Task<IEnumerable<CustomerBookDto>> GetAllCustomerBooks(long customerId)
+    public async Task<IEnumerable<ExtendBookDto>> GetAllCustomerBooks(long customerId)
     {
-        var query = @"SELECT * FROM customer_books
-                    WHERE customer_id = @customerId";
+        var query = @"SELECT
+                        b.book_id AS BookId,
+                        b.name AS Name,
+                        b.genre AS Genre,
+                        b.language AS Language,
+                        a.author_name AS AuthorName,
+                        p.publisher_name AS PublisherName,
+                        b.publish_date AS PublishDate,
+                        b.pages AS Pages
+                    FROM
+                        customer_books AS cb
+                    JOIN
+                        books AS b ON cb.book_id = b.book_id
+                    LEFT JOIN
+                        authors AS a ON b.author_id = a.author_id
+                    LEFT JOIN
+                        publishers AS p ON b.publisher_id = p.publisher_id
+                    WHERE
+                        cb.customer_id = @customerId";
         using var connection = _context.CreateConnection();
-        var books = await connection.QueryAsync<CustomerBookDto>(query, new { customerId });
+        var books = await connection.QueryAsync<ExtendBookDto>(query, new { customerId });
         return books.ToList();
     }
 
-    public async Task<CustomerBookDto> GetCustomerBook(long customerId, long bookId)
+    public async Task<ExtendBookDto> GetCustomerBook(long customerId, long bookId)
     {
-        var query = @"SELECT * FROM customer_books
-                    WHERE customer_id = @customerId
-                    AND book_id = @bookId";
+        var query = @"SELECT
+                        b.book_id AS BookId,
+                        b.name AS Name,
+                        b.genre AS Genre,
+                        b.language AS Language,
+                        a.author_name AS AuthorName,
+                        p.publisher_name AS PublisherName,
+                        b.publish_date AS PublishDate,
+                        b.pages AS Pages
+                    FROM
+                        customer_books AS cb
+                    JOIN
+                        books AS b ON cb.book_id = b.book_id
+                    LEFT JOIN
+                        authors AS a ON b.author_id = a.author_id
+                    LEFT JOIN
+                        publishers AS p ON b.publisher_id = p.publisher_id
+                    WHERE
+                        cb.customer_id = @customerId
+                        AND cb.book_id = @bookId";
         using var connection = _context.CreateConnection();
         var temp = new CustomerBook {BookId = bookId, CustomerId = customerId};
-        var book = await connection.QueryFirstOrDefaultAsync<CustomerBookDto>(query, temp);
+        var book = await connection.QueryFirstOrDefaultAsync<ExtendBookDto>(query, temp);
         return book;
     }
 }
