e6603e66fdbeb5d0283fa650e7e094fd145240bf
Roman Savenko <rsaven.wk@gmail.com>
2023-09-18 09:49:16 +0300

    feat: add enum extensions

---
+++ Entities/Enums/EnumExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace Entities.Enums;
+
+public class EnumExtensions
+{
+    public static bool IsEnumNameValid<T>(string enumName)
+    {
+        foreach (T name in Enum.GetValues(typeof(T)))
+        {
+            if (string.Equals(enumName, name.ToString(), StringComparison.OrdinalIgnoreCase))
+                return true;
+        }
+        return false;
+    }
+
+    public static T EnumNameToEnum<T>(string enumName, T defaultValue)
+    {
+        if (string.IsNullOrEmpty(enumName))
+            return defaultValue;
+        return (T)Enum.Parse(typeof(T), enumName, true);
+    }
+}
