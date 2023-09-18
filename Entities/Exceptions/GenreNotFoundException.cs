187f59cf427ce156cc84e8d253ebd08e8b404ed7
Roman Savenko <rsaven.wk@gmail.com>
2023-09-18 09:47:41 +0300

    feat: add enum exceptions

---
+++ Entities/Exceptions/GenreNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Enums;
+
+namespace Entities.Exceptions;
+
+public class GenreNotFoundException : NotFoundException
+{
+    public GenreNotFoundException(string genre) 
+    : base($"Book with genre: {genre} doesn't exist in the database.")
+    {
+    }
+
+    public GenreNotFoundException(Genre genre) 
+    : base($"Book with genre: {genre} doesn't exist in the database.")
+    {
+    }
+}
---
+++ Entities/Exceptions/LanguageNotFoundException.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Enums;
+
+namespace Entities.Exceptions;
+
+public class LanguageNotFoundException : NotFoundException
+{
+    public LanguageNotFoundException(string language) 
+    : base($"Book with language: {language} doesn't exist in the database.")
+    {
+    }
+
+    public LanguageNotFoundException(Language language) 
+    : base($"Book with language: {language} doesn't exist in the database.")
+    {
+    }
+}
