8cb301304b90cf2632cc61c0fe94764bb923e4ad
Roman Savenko <rsaven.wk@gmail.com>
2023-09-18 22:21:01 +0300

    refactor: update customer book service

--- Service.Contracts/ICustomerBookService.cs
+++ Service.Contracts/ICustomerBookService.cs
@@ -1,11 +1,12 @@
-using Shared.DataTransferObjects;
+using Shared.DataTransferObjects.Book;
+using Shared.DataTransferObjects.CustomerBook;
 
 namespace Service.Contracts;
 
 public interface ICustomerBookService
 {
-    Task<IEnumerable<CustomerBookDto>> GetAllCustomerBooks(long customerId);
-    Task<CustomerBookDto> GetCustomerBook(long customerId, long bookId);
+    Task<IEnumerable<ExtendBookDto>> GetAllCustomerBooks(long customerId);
+    Task<ExtendBookDto> GetCustomerBook(long customerId, long bookId);
     Task AddCustomerBook(long customerId, CustomerBookDto book);
     Task DeleteCustomerBook(long customerId, long bookId);
 }
--- Service/CustomerBookService .cs
+++ Service/CustomerBookService .cs
@@ -1,7 +1,8 @@
 using Contracts;
 using Entities.Exceptions;
 using Service.Contracts;
-using Shared.DataTransferObjects;
+using Shared.DataTransferObjects.Book;
+using Shared.DataTransferObjects.CustomerBook;
 
 namespace Service
 {
@@ -18,7 +19,9 @@ namespace Service
             throw new CustomerNotFoundException(customerId);
             if (!await _repositoryManager.Book.BookExists(book.BookId))
             throw new BookNotFoundException(book.BookId);
-            await _repositoryManager.CustomerBook.AddCustomerBook(customerId, book);
+            if (await _repositoryManager.CustomerBook.CustomerBookExists(customerId, book.BookId))
+            throw new CustomerBookConflictException(customerId, book.BookId);
+            _repositoryManager.CustomerBook.AddCustomerBook(customerId, book);
         }
 
         public async Task DeleteCustomerBook(long customerId, long bookId)
@@ -27,17 +30,17 @@ namespace Service
             throw new CustomerNotFoundException(customerId);
             if (!await _repositoryManager.Book.BookExists(bookId))
             throw new BookNotFoundException(bookId);
-            await _repositoryManager.CustomerBook.DeleteCustomerBook(customerId, bookId);
+            _repositoryManager.CustomerBook.DeleteCustomerBook(customerId, bookId);
         }
 
-        public async Task<IEnumerable<CustomerBookDto>> GetAllCustomerBooks(long customerId)
+        public async Task<IEnumerable<ExtendBookDto>> GetAllCustomerBooks(long customerId)
         {
             if (!await _repositoryManager.Customer.CustomerExists(customerId))
             throw new CustomerNotFoundException(customerId);
             return await _repositoryManager.CustomerBook.GetAllCustomerBooks(customerId);
         }
 
-        public async Task<CustomerBookDto> GetCustomerBook(long customerId, long bookId)
+        public async Task<ExtendBookDto> GetCustomerBook(long customerId, long bookId)
         {
             if (!await _repositoryManager.Customer.CustomerExists(customerId))
             throw new CustomerNotFoundException(customerId);
